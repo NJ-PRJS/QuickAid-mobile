9176d6382be31c10c2c1774d984999825e1b79db
DEOGASS WIDAMBE <87471672+widambeDeograss@users.noreply.github.com>
2023-09-22 21:22:23 +0300

    lng sc

---
+++ constants/Colors.ts
@@ -0,0 +1,5 @@
+export default {
+    primary: '#F94C81',
+    accent: '#6FA8DD',
+    secondary:'#B3D5F4'
+};
--- screens/ChooseLanguage.tsx
+++ screens/ChooseLanguage.tsx
@@ -1,32 +1,49 @@
 import * as React from 'react';
-import {View, Text, TouchableOpacity,Button } from 'react-native';
+import {View, Text, TouchableOpacity,Button, StyleSheet } from 'react-native';
+import { SCREEN_HEIGHT } from '../constants/UiData';
+import Colors from '../constants/Colors';
 
 
-const ChooseLanguageScreen = () => {
+const ChooseLanguageScreen = ({navigation}:any) => {
     const [choosenLanguage, setchoosenLanguage] = React.useState('ENGLISH')
 
 return(
-    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-     <Text style={{fontSize:18, fontFamily: 'Poppins-bold'}}>QuickAid</Text>
-     <Text style={{fontSize:13,  fontFamily: 'Poppins-bold', marginTop:12}}>Choose language</Text>
+    <View style={{flex:1, justifyContent:"center", alignItems:"center", height:SCREEN_HEIGHT*0.71}}>
+     <Text style={{fontSize:30, fontFamily: 'Poppins-bold'}}>QuickAid</Text>
+     <Text style={{fontSize:15,  fontFamily: 'Poppins-semibold', marginTop:3}}>Choose language</Text>
 
      <TouchableOpacity
-     style={{borderColor:'pink', width:"80%", borderRadius:1, }}
+     style={{borderColor:Colors.primary, width:"80%", borderRadius:5,height:50 , borderWidth:1, padding:5, marginTop:10}}
      >
-        SWAHILI
+        <Text style={{textAlign:"center", fontFamily:"Poppins-bold", alignItems:"center", marginTop:11}} >SWAHILI</Text>
      </TouchableOpacity>
 
      <TouchableOpacity
-     style={{borderColor:'pink', width:"80%", borderRadius:1}}
+     style={{ width:"80%", borderRadius:5,height:50 , borderWidth:1, padding:5, marginTop:5}}
      >
-        SWAHILI
+         <Text style={{textAlign:"center", fontFamily:"Poppins-bold", marginTop:11}} >ENGLISH</Text>
      </TouchableOpacity>
 
-     <View style={{marginTop:10}}>
-     <Button title='Next'/> 
+     <TouchableOpacity
+                style={styles.getStatedbtn}
+                onPress={() => navigation.navigate('onboarding')}>
+                <Text style={{ fontFamily: 'Poppins-bold', fontSize: 15, color:"white"}}>
+                  NEXT
+                </Text>
+              </TouchableOpacity>
      </View>
-    </View>
 )
 }
-
+const styles = StyleSheet.create({
+    getStatedbtn:{
+        height: 50,
+        borderRadius: 10,
+        backgroundColor:Colors.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation:10,
+        marginTop:25,
+        width:"80%"
+      }
+})
 export default ChooseLanguageScreen;
